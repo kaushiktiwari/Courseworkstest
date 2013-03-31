89b627e467eba5a5380fa9f4054a6343365eb592
kaushiktewari <kkt2123@columbia.edu>
2013-03-31 15:34:13 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Courseworks::Application.config.secret_token = 'd0ce22d67e4a0472f3af6dc44bfe38ed4cfb2a3cfbba6c05ed4efcd1720a7406cf4d844f3e0aa635b670603ca3a5d9a224e4d6266dca582562889f29e053997f'
